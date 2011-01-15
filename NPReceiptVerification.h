a7e7b14c6f25d60d9661fb3f8cfbe9a335a4ad4d
Nick Paulson <nick@nickpaulson.com>
2011-01-15 16:36:10 -0500

    First commit

---
+++ NPReceiptVerification.h
@@ -0,0 +1,15 @@
+//
+//  NPReceiptVerification.h
+//
+//  Created by Nick Paulson on 1/15/11.
+//  Copyright 2011 Linebreak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NPReceiptVerification : NSObject {
+
+}
+
+@end
